debbacaf0749946d7abd26acc39a619a48c3d871
Augusto Teixeira <aught.ceo@gmail.com>
2021-11-04 12:30:12 -0300

    some commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { useContext } from "react";
 import styles from "./App.module.scss";
 import { LoginBox } from "./component/LoginBox";
 import { MessageList } from "./component/MessageList";
+import { SendMessageForm } from "./component/SendMessageForm";
+import { AuthContext } from "./contexts/auth";
 
 export function App() {
+  const { user } = useContext(AuthContext);
   return (
-    <main className={styles.contentWrapper}>
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ""
+      }`}
+    >
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
